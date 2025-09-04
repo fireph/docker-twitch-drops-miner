0933df6fdc54806a6b6459904163fc95121b0788
fireph <443370+fireph@users.noreply.github.com>
2025-09-03 21:51:56 -0700

    chown mounted volumes to try to avoid permissions issues

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+if [ -d "/TwitchDropsMiner/config" ]; then
+    chown -R "$(id -u):$(id -g)" /TwitchDropsMiner/config
+fi
+if [ -d "/TwitchDropsMiner/cache" ]; then
+    chown -R "$(id -u):$(id -g)" /TwitchDropsMiner/cache
+fi
+
 exec /TwitchDropsMiner/TwitchDropsMiner
