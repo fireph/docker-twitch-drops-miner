19d8770638e38b1c9948850aa1fe313cb7f97508
fireph <443370+fireph@users.noreply.github.com>
2025-09-04 22:22:49 -0700

    Removing chown in startapp.sh
    doesn't seem to help much in solving any permissions issues

--- startapp.sh
+++ startapp.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-if [ -d "/TwitchDropsMiner/config" ]; then
-    chown -R "$(id -u):$(id -g)" /TwitchDropsMiner/config
-fi
-if [ -d "/TwitchDropsMiner/cache" ]; then
-    chown -R "$(id -u):$(id -g)" /TwitchDropsMiner/cache
-fi
-
 exec /TwitchDropsMiner/TwitchDropsMiner
